df1b8780245080bcc919f2ad2d6bff2e6d65d0f0
vanishmax <maxkors2014@gmail.com>
2019-05-24 23:15:57 +0300

    Adaptive header

--- components/header.js
+++ components/header.js
@@ -3,45 +3,63 @@ import Link from 'next/link'
 
 export default function Header () {
   return (
-    <header className="flex w-10-12-auto items-center justify-between bg-white p-3">
-      <div className="flex">
-        <Link prefetch href="/">
-          <img src="/static/headerMini.png" alt="header logo" className="cursor-pointer"/>
-        </Link>
+    <header className="app flex items-center justify-between p-3">
+      <div className="hidden lg:flex flex-grow items-center justify-between">
+        <div className="flex">
+          <Link prefetch href="/">
+            <img src="/static/headerMini.png" alt="header logo" className="cursor-pointer"/>
+          </Link>
+        </div>
+
+        <div className="flex justify-between text-gray-900 no-underline font-semibold text-base text-sm">
+          <div className="mr-10">
+            <Link prefetch href="/">
+              <a className=" py-2 pl-3 hover:text-gray-200">Events</a>
+            </Link>
+          </div>
+          <div className="mr-10">
+            <Link prefetch href="/">
+              <a className="py-2 pl-3 hover:text-gray-200">Clubs</a>
+            </Link>
+          </div>
+          <div className="mr-10">
+            <Link prefetch href="/">
+              <a className="py-2 pl-3 hover:text-gray-200">People</a>
+            </Link>
+          </div>
+          <div className="mr-10">
+            <Link prefetch href="/">
+              <a className="py-2 pl-3 hover:text-gray-200">Campus life</a>
+            </Link>
+          </div>
+          <div>
+            <Link prefetch href="/">
+              <a className="py-2 pl-3 hover:text-gray-200">Videos</a>
+            </Link>
+          </div>
+        </div>
+
+        <div className="flex justify-end">
+          <Link href="/">
+            <a className="text-gray-800 font-bold font-comforta no-underline" target="_blank">English</a>
+          </Link>
+        </div>
       </div>
 
-      <div className="flex justify-between text-gray-900 no-underline font-semibold text-base text-sm">
-        <div className="mr-10">
+      <div className="flex lg:hidden flex-grow items-center justify-between">
+        <div className="flex justify-start">
+          <img src="/static/menu.png" alt="menu icon"/>
+        </div>
+        <div className="flex flex-grow justify-center">
           <Link prefetch href="/">
-            <a className=" py-2 pl-3 hover:text-gray-200">Events</a>
+            <img src="/static/headerMini.png" alt="header logo" className="cursor-pointer"/>
           </Link>
         </div>
-        <div className="mr-10">
-          <Link prefetch href="/">
-            <a className="py-2 pl-3 hover:text-gray-200">Clubs</a>
+        <div className="flex justify-end">
+          <Link href="/">
+            <a className="text-gray-800 font-bold font-comforta no-underline" target="_blank">English</a>
           </Link>
         </div>
-        <div className="mr-10">
-          <Link prefetch href="/">
-            <a className="py-2 pl-3 hover:text-gray-200">People</a>
-          </Link>
-        </div>
-        <div className="mr-10">
-          <Link prefetch href="/">
-            <a className="py-2 pl-3 hover:text-gray-200">Campus life</a>
-          </Link>
-        </div>
-        <div>
-          <Link prefetch href="/">
-            <a className="py-2 pl-3 hover:text-gray-200">Videos</a>
-          </Link>
-        </div>
-      </div>
-
-      <div className="flex justify-end">
-        <Link href="/">
-          <a className="text-gray-800 font-bold font-comforta py-1 px-4 no-underline" target="_blank">English</a>
-        </Link>
       </div>
     </header>
   )
--- next.config.js
+++ next.config.js
@@ -1,5 +1,3 @@
 const withCSS = require('@zeit/next-css')
-const withBabelMinify = require('next-babel-minify')()
-const withPurgeCss = require('next-purgecss')
 
-module.exports = withBabelMinify(withCSS(withPurgeCss()))
+module.exports = withCSS({})
