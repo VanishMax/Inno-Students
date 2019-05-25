ac4cfff84366595e31bccdfa2532ff6c6f1d32aa
vanishmax <maxkors2014@gmail.com>
2019-05-25 09:44:54 +0300

    Another adaptive stylings

--- components/header.js
+++ components/header.js
@@ -41,16 +41,17 @@ export default function Header () {
 
         <div className="flex justify-end">
           <Link href="/">
-            <a className="mr-8 text-gray-800 font-bold no-underline" target="_blank">English</a>
+            <a className="mr-8 text-gray-800 font-bold no-underline hover:text-green-800" target="_blank">English</a>
           </Link>
           <Link href="/login">
-            <a className="text-gray-800 font-bold no-underline" target="_blank">Log In</a>
+            <a className="text-gray-800 font-bold no-underline hover:text-green-800" target="_blank">Log In</a>
           </Link>
         </div>
       </div>
 
+
       <div className="flex lg:hidden flex-grow items-center justify-between">
-        <div className="flex justify-start">
+        <div className="flex justify-start cursor-pointer">
           <img src="/static/menu.png" alt="menu icon"/>
         </div>
         <div className="flex flex-grow justify-center">
@@ -60,7 +61,13 @@ export default function Header () {
         </div>
         <div className="flex justify-end">
           <Link href="/">
-            <a className="text-gray-800 font-bold font-comforta no-underline" target="_blank">English</a>
+            <span className="cursor-pointer text-base text-gray-800 hover:text-green-800 font-bold no-underline">
+              <a className="hidden lg:inline mr-8" target="_blank">English</a>
+              <a className="inline lg:hidden" target="_blank">EN</a>
+            </span>
+          </Link>
+          <Link href="/login">
+            <a className="hidden lg:inline text-gray-800 hover:text-green-800 font-bold no-underline" target="_blank">Log In</a>
           </Link>
         </div>
       </div>
