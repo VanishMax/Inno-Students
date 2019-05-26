363865c8d1007ae7979fa9fbf9f199ad01c506a7
vanishmax <maxkors2014@gmail.com>
2019-05-26 13:44:02 +0300

    LogIn and SignUp pages localization

---
+++ langs/login.js
@@ -0,0 +1,60 @@
+const login = {
+  headSignTitle: {
+    en: 'Sign Up Page',
+    ru: 'Страница регистрации'
+  },
+  headSignDescr: {
+    en: 'Create a profile to get more features as an authorized user',
+    ru: 'Создайте профиль чтобы получить больше возвожостей в качестве зарегистрированного пользователя'
+  },
+  headLogTitle: {
+    en: 'Log In Page',
+    ru: 'Страница авторизации'
+  },
+  headLogDescr: {
+    en: 'Log In to get more features as an authorized user',
+    ru: 'Войдите чтобы получить больше возвожостей в качестве зарегистрированного пользователя'
+  },
+  titleLog: {
+    en: 'Log In',
+    ru: 'Войти'
+  },
+  titleSign: {
+    en: 'Sign Up',
+    ru: 'Регистрация'
+  },
+  signButton: {
+    en: 'Sign Up',
+    ru: 'Создать'
+  },
+  subtitle: {
+    en: 'Please, provide your info',
+    ru: 'Пожалуйста, заполните форму'
+  },
+  username: {
+    en: 'Username',
+    ru: 'Имя пользователя'
+  },
+  placeholder: {
+    en: 'Nickname12',
+    ru: 'Васёк'
+  },
+  password: {
+    en: 'Password',
+    ru: 'Пароль'
+  },
+  confirm: {
+    en: 'Confirm Password',
+    ru: 'Подтвердите пароль',
+  },
+  donthave: {
+    en: 'Don\'t have an account?',
+    ru: 'Еще нет аккаунта?'
+  },
+  already: {
+    en: 'Already have an account?',
+    ru: 'Уже есть аккаунт?'
+  }
+}
+
+export default login
