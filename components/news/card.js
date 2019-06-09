0965674fc4070f2f52ef4e29e9206d2ca0378b31
VanishMax <maxkors2014@gmail.com>
2019-06-09 12:24:44 +0300

    Important grid changes

--- components/news/card.js
+++ components/news/card.js
@@ -1,20 +1,22 @@
 import React from 'react'
 
-export default ({news, big, margin}) => {
-
+export default ({news, big, last}) => {
   return (
-    <div className={'news-card news-card-' + (big ? 'big' : 'small') + (margin ? ' md:mr-6 md:mb-0 mb-6' : '')}>
-      <div className="news-card-cover"
-           style={{backgroundImage: `url("${news.img}")`}}/>
-      <div className="news-card-overlay" />
-      <div className="news-card-caption">
-        <div className="small">
-          {news.top.map(top => (
-            <span key={top}>{top}</span>
-          ))}
+    <React.Fragment>
+      <div className={'news-card' + (last ? '' : ' md:mr-6 md:mb-0 mb-6') + (big ? ' big' : '')}>
+        <div className="news-card-cover"
+             style={{backgroundImage: `url("${news.img}")`}}/>
+        <div className="news-card-overlay" />
+        <div className="news-card-caption">
+          <div className="small">
+            {news.top.map(top => (
+              <span key={top}>{top}</span>
+            ))}
+          </div>
+          <div className="big">{news.title}</div>
         </div>
-        <div className="big">{news.title}</div>
       </div>
-    </div>
+      { last && <br className="fullBr" /> }
+    </React.Fragment>
   )
 }
--- components/news/grid.js
+++ components/news/grid.js
@@ -1,41 +1,14 @@
 import React from 'react'
 import NewsCard from './card'
 
-const NewsRow = ({data}) => {
-  return (
-    <div className="news-grid-row md:flex-row flex-col">
-      {data.length === 1 ?
-        <NewsCard news={data[0]} big />
-      :
-        <React.Fragment>
-          <NewsCard news={data[0]} margin />
-          <NewsCard news={data[1]} />
-        </React.Fragment>
-      }
-    </div>
-  )
-}
-
 export default ({data}) => {
-  let allNews = []
-  let i = 0
-  while(i !== data.length) {
-    if(i % 3 === 0) {
-      allNews.push(<NewsRow data={[data[i]]} />)
-    } else if(i % 3 === 1) {
-      allNews.push(<NewsRow data={[data[i], data[i + 1]]} />)
-      i++
-    }
-    i++;
-  }
-
   return (
     <div className="news-grid">
-      {allNews.map((row, i) => (
-        <div key={i}>
-          {row}
-        </div>
-      ))}
+      <div className="news-grid-row md:flex-row flex-col">
+        {data.map((news, i) => (
+          <NewsCard key={i} big={i % 3 === 0} news={news} last={i % 3 === 2 || i % 3 === 0} />
+        ))}
+      </div>
     </div>
   )
 }
