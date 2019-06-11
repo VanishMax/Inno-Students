37023b1409126340f9220f6335b2931a57bf42f8
VanishMax <maxkors2014@gmail.com>
2019-06-11 15:26:43 +0300

    Refactored new post form

---
+++ components/post/newPostForm.js
@@ -0,0 +1,54 @@
+import React from 'react'
+import Lang from '../../langs/newpost'
+import Layout from '../../layouts/user'
+
+const fakeUserImg = '/static/images/fakeUser.jpg'
+const bucket = 'http://inno-students.s3.amazonaws.com/'
+
+export default ({lang, user}) => {
+  return(
+    <Layout lang={lang} title={Lang.metatitle[lang]}
+            img={user.img !== '' ? bucket + user.img : fakeUserImg}
+            isAdmin={user.role === "A"}>
+      <form className="w-full max-w-sm mx-auto">
+        <div className="md:flex md:items-center mb-6">
+          <div className="md:w-1/3">
+            <label className="block text-gray-700 font-bold md:text-right mb-1 md:mb-0 pr-4"
+                   htmlFor="title-en">
+              {Lang.titleEn[lang]}
+            </label>
+          </div>
+          <div className="md:w-2/3">
+            <input
+              className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-green-300"
+              id="title-en" type="text" name="titleEn" placeholder="Name of the post" />
+          </div>
+        </div>
+        <div className="md:flex md:items-center mb-6">
+          <div className="md:w-1/3">
+            <label className="block text-gray-700 font-bold md:text-right mb-1 md:mb-0 pr-4"
+                   htmlFor="title-ru">
+              {Lang.titleRU[lang]}
+            </label>
+          </div>
+          <div className="md:w-2/3">
+            <input
+              className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-green-300"
+              id="title-ru" type="text" name="titleRu" placeholder="Название нового поста" />
+          </div>
+        </div>
+        <h3 className="mb-4 text-center text-base italic text-red-800">
+          Some error
+        </h3>
+        <div className="flex items-center justify-center">
+          <button
+            className="shadow bg-green-500 hover:bg-green-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded"
+            type="submit" name="submit"
+          >
+            {Lang.create[lang]}
+          </button>
+        </div>
+      </form>
+    </Layout>
+  )
+}
---
+++ langs/newpost.js
@@ -0,0 +1,20 @@
+const newPost = {
+  metatitle: {
+    en: 'New Post',
+    ru: 'Новый пост'
+  },
+  titleEn: {
+    en: 'Title (EN)',
+    ru: 'Название (EN)'
+  },
+  titleRU: {
+    en: 'Title (RU)',
+    ru: 'Название (RU)'
+  },
+  create: {
+    en: 'Create',
+    ru: 'Создать'
+  }
+}
+
+export default newPost
