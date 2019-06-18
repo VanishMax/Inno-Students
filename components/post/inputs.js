f0ad3bab4e576832ee7e761611bbd6323482d839
VanishMax <maxkors2014@gmail.com>
2019-06-18 12:37:06 +0300

    Change textarea by react-contenteditable

--- components/post/inputs.js
+++ components/post/inputs.js
@@ -1,27 +1,35 @@
 import React from 'react'
 
 import Lang from '../../langs/post'
+import ContentEditable from 'react-contenteditable'
 
 import dynamic from 'next/dynamic'
-const Dante = dynamic(
-  () => import('Dante2'), {ssr: false}
-)
-
+const Dante = dynamic(() => import('Dante2'), {ssr: false})
 import {ImageBlockConfig} from 'Dante2/package/lib/components/blocks/image'
 import {VideoBlockConfig} from 'Dante2/package/lib/components/blocks/video'
 import {DividerBlockConfig} from 'Dante2/package/lib/components/blocks/divider'
 
-export default ({lang, post, changeContent, content, isEdit}) => {
+export default ({lang, changeContent, form, changeForm, isEdit, titleRef, leadRef, clearPlaceholder}) => {
   return (
     <React.Fragment>
-      <textarea className="block w-full text-3xl md:text-5xl px-0 overflow-hidden"
-                placeholder={Lang.titlePlaceholder[lang]}  name="title"
-                value={post[lang].title} disabled={!isEdit} rows={1} />
-      <textarea className="block w-full text-xl md:text-xl text-gray-600"
-                value={post[lang].lead} rows={1} name="lead"
-                placeholder={Lang.leadPlaceholder[lang]} disabled={!isEdit} />
+      <ContentEditable
+        className="block w-full text-3xl md:text-5xl px-0 overflow-hidden focus:outline-none"
+        html={form.title}
+        disabled={!isEdit}
+        onFocus={(e) => clearPlaceholder(e, 'title')}
+        onChange={(e) => changeForm(e, 'title')}
+        innerRef={titleRef}
+      />
+      <ContentEditable
+        className="block w-full text-xl md:text-xl text-gray-600 overflow-hidden focus:outline-none"
+        html={form.lead}
+        disabled={!isEdit}
+        onFocus={(e) => clearPlaceholder(e, 'lead')}
+        onChange={(e) => changeForm(e, 'lead')}
+        innerRef={leadRef}
+      />
 
-      <Dante onChange={changeContent} content={content} read_only={!isEdit}
+      <Dante onChange={changeContent} content={form.content} read_only={!isEdit}
              widgets={[ImageBlockConfig({ options: { upload_url: '/post/urlll' } }),
                VideoBlockConfig({ options: { placeholder: 'Put an external video link', endpoint: '//open.iframe.ly/api/oembed?origin=https://github.com&url=', caption: 'optional caption', }, }),
                DividerBlockConfig()]}/>
--- pages/post.js
+++ pages/post.js
@@ -1,8 +1,9 @@
-import React, {useContext, useState} from 'react'
+import React, {useContext, useState, useRef} from 'react'
 import Head from 'next/head'
 
 import withPost from '../middleware/HOCs/withPost'
 import {LangContext} from '../middleware/context'
+import Lang from '../langs/post'
 
 import Unexisting from '../components/post/unexisting'
 import Actions from '../components/post/actions'
@@ -14,21 +15,36 @@ const Post = ({post, user, isAuthor}) => {
   const lang = useContext(LangContext)
   const [isEdit, goToEdit] = useState(false)
   const [isSnak, changeSnack] = useState(null)
-  const [content, changeDraft] = useState(null)
+
+  const titleRef = useRef(null)
+  const leadRef = useRef(null)
+
+  const [form, editForm] = useState({
+    title: post[lang].title || Lang.titlePlaceholder[lang],
+    lead: post[lang].lead || Lang.leadPlaceholder[lang],
+    content: post[lang].content || null
+  })
+  const changeForm = (e, name) => {
+    editForm({...form, [name]: e.target.value})
+  }
+  const clearPlaceholder = (e, name) => {
+    if(e.target.textContent.indexOf(Lang[name + 'Placeholder'][lang]) !== -1)
+      editForm({...form, [name]: ''})
+  }
   const changeContent = (content) => {
     // console.log(content.emitSerializedOutput())
-    changeDraft(content.emitSerializedOutput())
+    editForm({...form, content: content.emitSerializedOutput()})
   }
 
   const edit = () => {
     if(isEdit) {
       goToEdit(false)
       changeSnack('You\'ve quitted the editing mode')
-      setTimeout(() => changeSnack(null), 5000)
+      setTimeout(() => changeSnack(null), 3000)
     } else {
       goToEdit(true)
       changeSnack('You\'re now in editing mode')
-      setTimeout(() => changeSnack(null), 5000)
+      setTimeout(() => changeSnack(null), 3000)
     }
   }
 
@@ -53,7 +69,9 @@ const Post = ({post, user, isAuthor}) => {
 
         <div className="content">
           <PostHeader lang={lang} post={post} />
-          <Inputs lang={lang} post={post} isEdit={isEdit} content={content} changeContent={changeContent} />
+          <Inputs lang={lang} isEdit={isEdit} form={form}
+                  titleRef={titleRef} leadRef={leadRef} clearPlaceholder={clearPlaceholder}
+                  changeForm={changeForm} changeContent={changeContent} />
         </div>
       </React.Fragment>
     )
