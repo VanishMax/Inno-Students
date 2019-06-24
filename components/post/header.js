4ac2cea38363bbb31b3cbda59b7c676c54e5014a
VanishMax <maxkors2014@gmail.com>
2019-06-24 18:26:05 +0300

    Add pretty date formatting

--- components/post/header.js
+++ components/post/header.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import prettyDate from '../../middleware/prettyDate'
 import {Eye, Comments, Clock} from '../icons/postHeader'
 import tags from '../../constants/tags'
 
@@ -11,12 +12,12 @@ export default ({post, lang}) => {
     <div className="text-sm text-gray-600">
       <span className="mr-4">{post.author[lang].name + ' ' + post.author[lang].surname}</span>
       <span className="mr-4">
-        <Icon width={16} height={16} className="post-top-icon" />&nbsp;
+        <Icon width={18} height={18} className="post-top-icon" />&nbsp;
         {post.tag}
       </span>
       <span className="mr-4">
         <Clock width={16} height={16} className="post-top-icon" />&nbsp;
-        {post.publishTime || post.creationDate}
+        {prettyDate(post.publishTime, lang) || post.creationDate}
       </span>
       <span className="mr-4">
         <Comments width={16} height={16} className="post-top-icon" />&nbsp;
---
+++ middleware/prettyDate.js
@@ -0,0 +1,19 @@
+import moment from 'moment'
+
+export default (date, lang) => {
+  let parsed = moment(date, 'YYYY-MM-DD HH:mm')
+  let output = ''
+  if (moment().diff(parsed, 'seconds') < 60) {
+    output = lang === 'ru' ? 'сейчас' : 'now'
+  } else if (moment().diff(parsed, 'minutes') < 60) {
+    output = moment().diff(parsed, 'minutes') + (lang === 'ru' ? ' минут назад' : ' minutes ago')
+  } else if (moment().diff(parsed, 'hours') < 12 || parsed.isSame(moment(), 'day')) {
+    output = parsed.locale(lang).fromNow()
+  } else if(parsed.isSame(moment(), 'year')) {
+    output = parsed.locale(lang).format('MMM, DD')
+  } else {
+    output = parsed.locale(lang).format('MMM YY, DD')
+  }
+
+  return output
+}
