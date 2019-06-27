46afc900008754f13e31230a6243754d324d3300
VanishMax <maxkors2014@gmail.com>
2019-06-27 14:57:28 +0300

    Change social links

--- components/socials.js
+++ components/socials.js
@@ -7,31 +7,21 @@ export default ({size}) => {
   const classes = `mr-2 w-${size} h-${size}`
   return (
     <React.Fragment>
-      <Link href="https://t.me/InnoStudents">
-        <a className={classes} target="_blank">
-          <Telegram />
-        </a>
-      </Link>
-      <Link href="https://vk.com/InnoStudents">
-        <a className={classes} target="_blank">
-          <Vkontakte />
-        </a>
-      </Link>
-      <Link href="https://instagram.com/InnoStudents">
-        <a className={classes} target="_blank">
-          <Instagram />
-        </a>
-      </Link>
-      <Link href="https://facebook.com/InnopolisStudents">
-        <a className={classes} target="_blank">
-          <FaceBook />
-        </a>
-      </Link>
-      <Link href="https://www.youtube.com/channel/UCwmOq5S4wwmycTgcxUT0-eQ">
-        <a target="_blank" className={classes}>
-          <YouTube />
-        </a>
-      </Link>
+      <a href="https://t.me/InnoStudents" className={classes} target="_blank">
+        <Telegram />
+      </a>
+      <a href="https://vk.com/InnoStudents" className={classes} target="_blank">
+        <Vkontakte />
+      </a>
+      <a href="https://instagram.com/InnoStudents" className={classes} target="_blank">
+        <Instagram />
+      </a>
+      <a href="https://facebook.com/InnopolisStudents" className={classes} target="_blank">
+        <FaceBook />
+      </a>
+      <a href="https://www.youtube.com/channel/UCwmOq5S4wwmycTgcxUT0-eQ" target="_blank" className={classes}>
+        <YouTube />
+      </a>
     </React.Fragment>
   )
 }
