8330d6045db23d06009c0e88d505e5692592f9bc
VanishMax <maxkors2014@gmail.com>
2019-06-30 17:45:14 +0300

    Fix language check in the server

--- middleware/HOCs/withLang.js
+++ middleware/HOCs/withLang.js
@@ -9,20 +9,11 @@ export default Page => {
   WithLang.getInitialProps = async context => {
     const ctx = context.ctx
 
-    // Find the language from query
+    // Find the language from query or cookie
     let lang = 'en'
     const { cookieLang } = cookies(ctx)
-    if((ctx.query && ctx.query.lang === 'ru') || cookieLang === 'ru') lang = 'ru'
-
-    // Redirect if russian cookies exist but query does not
-    // if(cookieLang === 'ru' && ctx.query.lang !== 'ru') {
-    //   if (ctx.res) {
-    //     ctx.res.writeHead(302, { Location: ctx.pathname + makeURLWithQuery({...ctx.query, lang: 'ru'})})
-    //     ctx.res.end()
-    //   } else {
-    //     Router.replace({ pathname: Router.pathname, query: { ...ctx.query, lang: 'ru' }, shallow: true})
-    //   }
-    // }
+    if(cookieLang === 'ru') lang = 'ru'
+    if(ctx.req && ctx.req.query.lang && ctx.req.query.lang === 'ru') lang = 'ru'
 
     return {
       ...(Page.getInitialProps ? await Page.getInitialProps(context) : {}),
